93ae7ea9c2482820a03f3f27b281fa52d8fd3719
Ryan Goce <ryangoce@gmail.com>
2023-09-05 11:16:29 +0800

    fix: added more stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,26 @@
 
+import { setup } from '@storybook/vue3';
+
+import { createPinia } from 'pinia';
+
 import '../src/index.css';
 
-//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+//👇 Registers a global Pinia instance inside Storybook to be consumed by existing stories
+setup((app) => {
+  app.use(createPinia());
+});
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+ parameters: {
+   actions: { argTypesRegex: '^on[A-Z].*' },
+   controls: {
+     matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/,
+     },
+   },
+ },
 };
 
 export default preview;
