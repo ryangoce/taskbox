9107446ce7048854e7fec3e9d91f2ecda8e96998
Ryan Goce <ryangoce@gmail.com>
2023-09-07 07:36:00 +0800

    test: added interactive test on load

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -3,6 +3,11 @@ import Task from './Task.vue';
 
 import { action } from '@storybook/addon-actions';
 
+
+import { userEvent, waitFor, within, fireEvent } from '@storybook/testing-library';
+
+import { expect } from '@storybook/jest';
+
 export default {
   component: Task,
   title: 'Task',
@@ -25,10 +30,16 @@ export const Default = {
   args: {
     task: {
       id: '1',
-      title: 'Test Task',
+      title: 'Test Task 2',
       state: 'TASK_INBOX',
     },
   },
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    const element = canvas.getByLabelText('Test Task 2')
+
+    await expect(element).toBeVisible()
+  }
 };
 
 export const Pinned = {
@@ -37,7 +48,7 @@ export const Pinned = {
       ...Default.args.task,
       state: 'TASK_PINNED',
     },
-  },
+  }
 };
 
 export const Archived = {
